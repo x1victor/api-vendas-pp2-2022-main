1a36a9ca059e2a887e90379ed9d300e23661ffa8
Victor Silva <victorhugosantossilva30@gmail.com>
2022-09-05 20:25:16 -0300

    05/09/22

--- api-vendas-pp2-2022-main/src/modules/products/routes/routes.Product.ts
+++ api-vendas-pp2-2022-main/src/modules/products/routes/routes.Product.ts
@@ -7,7 +7,16 @@ const routerProduct = Router()
 // cria um objeto controller
 const controllerProduct = new ProductController()
 
+
 // está criada a rota para inserir um produto no banco de dados
 routerProduct.post('/', controllerProduct.create)
+routerProduct.get('/', controllerProduct.list)
+routerProduct.get('/:id', controllerProduct.show)
+routerProduct.delete('/:id', controllerProduct.delete)
+routerProduct.put('/:id', controllerProduct.update)
+
+
+
+
 
 export default routerProduct
